fc472268805158f4de1560e4a8f710503e4f827f
Benson Waweru <bensonkogi20@gmail.com>
2025-11-07 21:08:51 +0300

    fix/add-starter-files

---
+++ src/assets/export.js
@@ -0,0 +1,100 @@
+import { IoMdCloudDone } from "react-icons/io";
+import { SiCyberdefenders } from "react-icons/si";
+import { FaUserEdit } from "react-icons/fa";
+import project1 from  '../src/assets/project1.jpg'
+import project2 from  '../src/assets/project2.jpg'
+import project3 from  '../src/assets/project3.jpg'
+import project4 from  '../src/assets/project4.jpg'
+
+export const services = [
+  {
+    icon: IoMdCloudDone,
+    title:'Cloud Solutions',
+    para:'Sagittis donec cursus sed pretium varius non sagittis ut. Amet mi augue purus malesuada pulvinar.',
+  },
+  {
+    icon: SiCyberdefenders,
+    title:'Cybersecurity',
+    para:'Sagittis donec cursus sed pretium varius non sagittis ut. Amet mi augue purus malesuada pulvinar.',
+  },
+  {
+    icon: FaUserEdit,
+    title:'IT Consulting',
+    para:'Sagittis donec cursus sed pretium varius non sagittis ut. Amet mi augue purus malesuada pulvinar.',
+  }
+]
+
+
+export const projects = [
+  {
+    img: project1,
+    title: 'Cloud Migration for TechCorp',
+    para: 'Augue purus malesuada pulvinar faucibus vel id hac. Sed duis sit vitae platea nulla nunc egestas pellentesque et.',
+  },
+  {
+    img: project2,
+    title: 'Cloud Migration for TechCorp',
+    para: 'Augue purus malesuada pulvinar faucibus vel id hac. Sed duis sit vitae platea nulla nunc egestas pellentesque et.',
+  },
+  {
+    img: project3,
+    title: 'Cloud Migration for TechCorp',
+    para: 'Augue purus malesuada pulvinar faucibus vel id hac. Sed duis sit vitae platea nulla nunc egestas pellentesque et.',
+  },
+  {
+    img: project4,
+    title: 'Cloud Migration for TechCorp',
+    para: 'Augue purus malesuada pulvinar faucibus vel id hac. Sed duis sit vitae platea nulla nunc egestas pellentesque et.',
+  },
+]
+
+export const reviews = [
+  {
+    rating:'4.8',
+    about:'Feugiat malesuada convallis egestas bibendum. Justo sit mattis sit suspendisse non fermentum egestas morbi sit. Pretium placerat elementum dignissim vel.',
+    name:'John Doe',
+    role: 'software developer'
+  },
+  {
+    rating:'3.5',
+    about:'Augue purus malesuada pulvinar faucibus vel id hac. Sed duis sit vitae platea nulla nunc egestas pellentesque et',
+    name:'Robert Doe',
+    role: 'android developer'
+  },
+  {
+    rating:'4.2',
+    about:'Feugiat malesuada convallis egestas bibendum. Justo sit mattis sit suspendisse non fermentum egestas morbi sit. Pretium placerat elementum dignissim vel.',
+    name:'Steve Kingson',
+    role: 'UI/UX designer'
+  },
+  {
+    rating:'3.9',
+    about:'Augue purus malesuada pulvinar faucibus vel id hac. Sed duis sit vitae platea nulla nunc egestas pellentesque et',
+    name:'Alex Grim',
+    role: 'Web Developer'
+  },
+  {
+    rating:'4.7',
+    about:'Feugiat malesuada convallis egestas bibendum. Justo sit mattis sit suspendisse non fermentum egestas morbi sit. Pretium placerat elementum dignissim vel.',
+    name:'Supria Sakura',
+    role: 'Head Designer'
+  },
+  {
+    rating:'4.8',
+    about:'Augue purus malesuada pulvinar faucibus vel id hac. Sed duis sit vitae platea nulla nunc egestas pellentesque et',
+    name:'John Doe',
+    role: 'software developer'
+  },
+  {
+    rating:'4.8',
+    about:'Feugiat malesuada convallis egestas bibendum. Justo sit mattis sit suspendisse non fermentum egestas morbi sit. Pretium placerat elementum dignissim vel.',
+    name:'John Doe',
+    role: 'software developer'
+  },
+  {
+    rating:'4.8',
+    about:'Augue purus malesuada pulvinar faucibus vel id hac. Sed duis sit vitae platea nulla nunc egestas pellentesque et',
+    name:'John Doe',
+    role: 'software developer'
+  },
+]
